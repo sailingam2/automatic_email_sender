b606a7343ff71685b0bfaf460f67d64f657a6b87
midhunlingam <mlingam@asu.edu>
2018-03-21 08:43:24 -0700

    Email Sender Python Code

---
+++ EmailSender.py
@@ -0,0 +1,148 @@
+import httplib2
+import os
+import oauth2client
+from oauth2client import client, tools
+import base64
+import mimetypes
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from apiclient import errors, discovery
+from email.mime.image import MIMEImage
+from email.mime.audio import MIMEAudio
+from email.mime.base import MIMEBase
+
+
+names = []
+emailID = []
+CompanyList = []
+SCOPES = 'https://www.googleapis.com/auth/gmail.send'
+CLIENT_SECRET_FILE = 'client_secret.json'
+APPLICATION_NAME = 'Gmail API Python Send Email'
+
+def get_credentials():
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir,
+                                   'gmail-python-email-send.json')
+    store = oauth2client.file.Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        credentials = tools.run_flow(flow, store)
+        print('Storing credentials to ' + credential_path)
+    return credentials
+
+def SendMessage(sender, to, subject, msgHtml, attachmentFile=None):
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    service = discovery.build('gmail', 'v1', http=http)
+    if attachmentFile:
+        message1 = createMessageWithAttachment(sender, to, subject, msgHtml, attachmentFile)
+    else:
+        message1 = CreateMessageHtml(sender, to, subject, msgHtml)
+    result = SendMessageInternal(service, "me", message1)
+    return result
+
+def SendMessageInternal(service, user_id, message):
+    try:
+        message = (service.users().messages().send(userId=user_id, body=message).execute())
+        print('Message Id: %s' % message['id'])
+        return message
+    except errors.HttpError as error:
+        print('An error occurred: %s' % error)
+        return "Error"
+    return "OK"
+
+def CreateMessageHtml(sender, to, subject, msgHtml):
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = subject
+    msg['From'] = sender
+    msg['To'] = to
+    # msg.attach(MIMEText(msgHtml, 'html'))
+    return {'raw': base64.urlsafe_b64encode(msg.as_string())}
+
+def createMessageWithAttachment(
+    sender, to, subject, msgHtml, attachmentFile):
+    """Create a message for an email.
+
+    Args:
+      sender: Email address of the sender.
+      to: Email address of the receiver.
+      subject: The subject of the email message.
+      msgHtml: Html message to be sent
+      msgPlain: Alternative plain text message for older email clients
+      attachmentFile: The path to the file to be attached.
+
+    Returns:
+      An object containing a base64url encoded email object.
+    """
+    message = MIMEMultipart('mixed')
+    message['to'] = to
+    message['from'] = sender
+    message['subject'] = subject
+
+    messageA = MIMEMultipart('alternative')
+    messageR = MIMEMultipart('related')
+
+    messageR.attach(MIMEText(msgHtml, 'html'))
+    messageA.attach(messageR)
+
+    message.attach(messageA)
+
+    print("create_message_with_attachment: file: %s" % attachmentFile)
+    content_type, encoding = mimetypes.guess_type(attachmentFile)
+
+    if content_type is None or encoding is not None:
+        content_type = 'application/octet-stream'
+    main_type, sub_type = content_type.split('/', 1)
+    if main_type == 'text':
+        fp = open(attachmentFile, 'rb')
+        msg = MIMEText(fp.read(), _subtype=sub_type)
+        fp.close()
+    elif main_type == 'image':
+        fp = open(attachmentFile, 'rb')
+        msg = MIMEImage(fp.read(), _subtype=sub_type)
+        fp.close()
+    elif main_type == 'audio':
+        fp = open(attachmentFile, 'rb')
+        msg = MIMEAudio(fp.read(), _subtype=sub_type)
+        fp.close()
+    else:
+        fp = open(attachmentFile, 'rb')
+        msg = MIMEBase(main_type, sub_type)
+        msg.set_payload(fp.read())
+        fp.close()
+    filename = os.path.basename(attachmentFile)
+    msg.add_header('Content-Disposition', 'attachment', filename=filename)
+    message.attach(msg)
+
+    return {'raw': base64.urlsafe_b64encode(message.as_string())}
+
+
+def listSplit(list):
+	string = list[0]
+	return string.split()
+
+with open('ListName.txt') as NameFile:
+    names = NameFile.readlines()
+
+with open('ListEmail.txt') as EmailFile:
+    emailID = EmailFile.readlines()
+    emailID = listSplit(emailID)
+
+with open('ListCompany.txt') as CompanyFile:
+    CompanyList = CompanyFile.readlines()
+	
+def main():
+	sender = "bharathkurapati44@gmail.com"
+	for emails, company in zip(emailID, CompanyList):
+		print emails
+		subject = 'Application for full-time opportunities with ' + company[:-1]
+		msgHtml = 'Dear Recruiter, <br/><br/>I hope this email finds you at the best.I am a graduate student from Arizona State University currently looking for full-time opportunities. I found your contact details while networking with peers on Linkedin and thought I should reach out to you on the available positions with your firm. My interests closely align with the role of Industrial Engineer. I am currently pursuing my Master\'s in Industrial Engineering at Arizona State University with concentration in Industrial Statistics and Lean Manufacturing. I am attaching my resume to this email for your consideration. I am sure your time spent on my profile is worthwhile. Looking forward to a future conversation with you!<br/><br/>Thanks and Regards,<br/>Bharath Krishna,<br/>M.S Industrial Engineering<br/>Arizona State University,<br/>480-277-1051.'
+		SendMessage(sender, emails, subject, msgHtml, 'resume.pdf')
+
+if __name__ == '__main__':
+    main()
